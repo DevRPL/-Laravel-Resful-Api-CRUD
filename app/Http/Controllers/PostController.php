b96026080915ff96fff8373e192513c181d61aa3
Muhamad Jamil Firdaus <muhamadjamil668@gmail.com>
2024-02-23 14:04:21 +0700

    Upload File (Image)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\PostDetailResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -28,8 +29,17 @@ class PostController extends Controller
         $request->validate([
             'title' => 'required|max:255',
             'news_content' => 'required',
+            'file' => 'image|max:2048|mimes:jpeg,png,jpg,gif'
         ]);
 
+        $image = null;
+        if ($request->file) {
+            $fileName = $this->generateRandomString();
+            $extension = $request->file->extension();
+            $image = $fileName.'.'.$extension;
+            Storage::putFileAs('image', $request->file, $image);
+        }
+        $request['image'] = $image;
         $request['author'] = Auth::user()->id;
         $post = Post::create($request->all());
 
@@ -55,5 +65,15 @@ class PostController extends Controller
         $post->delete();
 
         return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    function generateRandomString($length = 30) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
